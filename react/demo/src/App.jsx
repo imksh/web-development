6d9082a20e4b5e89f7e44157eaf36c1fe78302a1
Karan Sharma <karan03945@gmail.com>
2025-12-25 14:48:24 +0530

    react first project

---
+++ react/demo/src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function App() {
+  let a = 5;
+  let b = 10;
+  console.log(a + b);
+  return (
+    <>
+      <h1 id="head">Hello world</h1>
+      <div>this is my first react app</div>
+    </>
+  );
+}
+
+export default App;
